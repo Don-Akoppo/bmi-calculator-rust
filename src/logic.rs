474749d649a288804625a85f1fb8b363ffe61901
Don-Akoppo <ogannisyanakop150@gmail.com>
2026-04-25 12:12:23 +0300

    initial commit

---
+++ src/logic.rs
@@ -0,0 +1,15 @@
+pub struct BmiInput {
+    pub weight: u32,
+    pub height_cm: u32,
+}
+
+pub enum BmiError {
+    InvalidWeight,
+    InvalidHeight,
+}
+pub fn CalculateBmi(data: BmiInput) -> f64 {
+    let weight_kg: f64 = data.weight as f64;
+    let height_m: f64 = (data.height_cm as f64) / 100;
+
+    weight_kg / (height_m * height_m)
+}
