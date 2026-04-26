e62a160dc2314b42f91b75de44480083abfe7742
Don-Akoppo <ogannisyanakop150@gmail.com>
2026-04-26 18:32:29 +0300

    fix: resolve header styling issue

--- src/logic.rs
+++ src/logic.rs
@@ -7,9 +7,9 @@ pub enum BmiError {
     InvalidWeight,
     InvalidHeight,
 }
-pub fn CalculateBmi(data: BmiInput) -> f64 {
+pub fn calculate_bmi(data: BmiInput) -> f64 {
     let weight_kg: f64 = data.weight as f64;
-    let height_m: f64 = (data.height_cm as f64) / 100;
+    let height_m: f64 = data.height_cm as f64 * 0.01;
 
     weight_kg / (height_m * height_m)
 }
