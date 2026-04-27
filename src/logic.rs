985ab924beae693b16be6fd50089a51536b47bde
Don-Akoppo <ogannisyanakop150@gmail.com>
2026-04-27 21:14:57 +0300

    feat: add bmi calculation logic

--- src/logic.rs
+++ src/logic.rs
@@ -2,14 +2,22 @@ pub struct BmiInput {
     pub weight: u32,
     pub height_cm: u32,
 }
-
+#[derive(Debug)]
 pub enum BmiError {
     InvalidWeight,
     InvalidHeight,
 }
-pub fn calculate_bmi(data: BmiInput) -> f64 {
+pub fn calculate_bmi(data: BmiInput) -> Result<f64, BmiError> {
+    if data.weight > 700 || data.weight < 10 {
+        return Err(BmiError::InvalidWeight);
+    }
+
+    if data.height_cm > 290 || data.height_cm < 50 {
+        return Err(BmiError::InvalidHeight);
+    }
+
     let weight_kg: f64 = data.weight as f64;
     let height_m: f64 = data.height_cm as f64 * 0.01;
 
-    weight_kg / (height_m * height_m)
+    Ok(weight_kg / (height_m * height_m))
 }
