a1dfddd06aaf66b57836e4e671b40f1c333f3c33
Don-Akoppo <ogannisyanakop150@gmail.com>
2026-05-01 15:08:07 +0300

    feat: connect bmi logic to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 mod logic;
 
-fn main() {}
+use logic::{BmiError, BmiInput, calculate_bmi, get_category};
+
+fn main() -> Result<(), BmiError> {
+    let input = BmiInput {
+        weight: 75,
+        height_cm: 180,
+    };
+
+    let bmi = calculate_bmi(input)?;
+    let category = get_category(bmi);
+
+    println!("---Result---");
+    println!("Body Mass Index: {:.2}", bmi);
+    println!("Your category: {:?}", category);
+    Ok(())
+}
